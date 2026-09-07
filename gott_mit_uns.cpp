6a634497ad52c4e11768491f1612fdf68306a42a
Ulisses Gomes <ulissesgomesmacedo2010@gmail.com>
2026-09-07 17:02:07 -0300

    Add files via upload

---
+++ gott_mit_uns.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+int main () {
+
+
+int numero1, numero2;
+int vibb;
+
+cout << "diga o primeiro numero para a operacao \n";
+cin >> numero1;
+cout << "diga o segundo numero para a operacao \n";
+cin >> numero2;
+cout << "qual operacao sera realizada?\n";
+cout << "adicao (+), digite 1\n\n"
+        "subtracao (-), digite 2\n\n"
+        "divisao (/), digite 3\n\n"
+        "multiplicacao (x), digite 4\n\n";
+        cin >> vibb;
+        
+        if (vibb ==1 ) {
+          cout   << "a adicao dos numeros e: " << numero1 + numero2 << "\n";
+        } else if (vibb ==2) {
+            cout << "a subtracao dos numeros e: " << numero1 - numero2 << "\n";
+        } else if (vibb ==3) {
+            cout << "a divisao dos numeros e: " << numero1 / numero2 << "\n";
+            if (numero2 ==0) cout << "erro, dividido por 0\n";
+            return 0;
+        } else if (vibb ==4) {
+            cout << "a multiplicacao dos numeros e: " << numero1 * numero2 << "\n";
+        }
+         }  
