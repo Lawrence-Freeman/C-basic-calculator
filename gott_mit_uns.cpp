031eb7ccd6e5a22b54bc72d43c29ff2add55deb2
Ulisses Gomes <ulissesgomesmacedo2010@gmail.com>
2026-09-12 09:25:04 -0300

    Refactor calculator to allow repeated operations
    Added functionality to repeat operations based on user input.

--- gott_mit_uns.cpp
+++ gott_mit_uns.cpp
@@ -1,34 +1,42 @@
 #include <iostream>
+#include <cstdlib>
 using namespace std;
 int main () {
+    double numero1, numero2;
+    int vibb;
+    char retorno;
 
+inicio:
+    cout << "diga o primeiro numero para a operacao \n";
+    cin >> numero1;
+    cout << "diga o segundo numero para a operacao \n";
+    cin >> numero2;
+    cout << "qual operacao sera realizada?\n";
+    cout << "adicao (+), digite 1\n\n"
+            "subtracao (-), digite 2\n\n"
+            "divisao (/), digite 3\n\n"
+            "multiplicacao (x), digite 4\n\n";
+    cin >> vibb;
 
-double numero1, numero2;
-int vibb;
-
-cout << "diga o primeiro numero para a operacao \n";
-cin >> numero1;
-cout << "diga o segundo numero para a operacao \n";
-cin >> numero2;
-cout << "qual operacao sera realizada?\n";
-cout << "adicao (+), digite 1\n\n"
-        "subtracao (-), digite 2\n\n"
-        "divisao (/), digite 3\n\n"
-        "multiplicacao (x), digite 4\n\n";
-        cin >> vibb;
-        
-        if (vibb ==1 ) {
-          cout   << "a adicao dos numeros e: " << numero1 + numero2 << "\n";
-        } else if (vibb ==2) {
-            cout << "a subtracao dos numeros e: " << numero1 - numero2 << "\n";
-        } else if (vibb ==3) {
+    if (vibb == 1) {
+        cout << "a adicao dos numeros e: " << numero1 + numero2 << "\n";
+    } else if (vibb == 2) {
+        cout << "a subtracao dos numeros e: " << numero1 - numero2 << "\n";
+    } else if (vibb == 3) {
+        if (numero2 == 0) {
+            cout << "erro, divisao por zero\n";
+        } else {
             cout << "a divisao dos numeros e: " << numero1 / numero2 << "\n";
-           if (numero2 == 0) {
-    cout << "erro, divisao por zero\n";
-} else {
-    cout << "a divisao dos numeros e: " << numero1 / numero2 << "\n";
-}
-        } else if (vibb ==4) {
-            cout << "a multiplicacao dos numeros e: " << numero1 * numero2 << "\n";
         }
-    } 
+    } else if (vibb == 4) {
+        cout << "a multiplicacao dos numeros e: " << numero1 * numero2 << "\n";
+    }
+
+    cout << "\nDigitar outras notas?[s/n]\n";
+    cin >> retorno;
+    if (retorno == 's' || retorno == 'S') {
+        goto inicio;
+    }
+
+    return 0;
+}
